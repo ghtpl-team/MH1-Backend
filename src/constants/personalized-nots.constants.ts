0cccc34472637bb9bd0d06a81d480959b8170673
KeiTsukishima <gurkirat@freecustomer.in>
2024-10-08 18:35:09 +0530

    Fix: note colors

---
+++ src/constants/personalized-nots.constants.ts
@@ -0,0 +1,10 @@
+export const PersonalisedCardColor = {
+  'Scan Card': '#B7CCF0',
+  'Session Card': '#F4DD95',
+  Baby: '#CBC8F4',
+  BabyBottom: '#BDB9F7',
+  Body: '#F4DD95',
+  BodyBottom: '#EED485',
+  Checklist: '#F6BDDE',
+  ChecklistBottom: '#F8ABD8',
+};
